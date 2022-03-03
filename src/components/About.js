79ab941ea230530a4ad8b7e609f976d92167fdbb
isaac petersen <isaac.petersen5@gmail.com>
2022-03-02 17:49:09 -0800

    about and project portfollio component layed out

---
+++ src/components/About.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+function About() {
+  return (
+    <section>
+      <div>
+        <center>
+          <img src={profileImage} alt="picture of me"></img>
+        </center>
+        <p>Isaac Petersen</p>
+        <h2>About me</h2>
+        <p>
+          I'm baby esse green juice celiac shoreditch 90's asymmetrical
+          letterpress hashtag kickstarter food truck est quis. Try-hard street
+          art taiyaki dolore. Ugh raclette scenester, XOXO tilde craft beer yr
+          lo-fi cray. Shabby chic franzen selvage occupy VHS prism, air plant
+          locavore twee tumblr elit sustainable banh mi umami. Whatever austin
+          chambray taiyaki hot chicken. Tote bag la croix coloring book,
+          humblebrag officia ut est artisan shaman health goth woke. Beard
+          fingerstache VHS, flannel ut proident marfa deep v magna excepteur ad
+          aliquip reprehenderit. Snackwave duis slow-carb hammock, velit ea four
+          loko vape cillum adaptogen roof party drinking vinegar. Jianbing anim
+          shoreditch, fam semiotics vice cardigan. Proident viral chillwave,
+          paleo banjo next level fingerstache cred sunt kitsch cillum literally.
+          Before they sold out pour-over pinterest, wolf adaptogen tumblr
+          readymade. Asymmetrical meh brunch kale chips XOXO. Kickstarter
+          flexitarian thundercats tattooed gastropub cardigan tilde pork belly
+          neutra. Franzen sunt hell of waistcoat drinking vinegar paleo est
+          activated charcoal. Kale chips everyday carry hot chicken skateboard
+          non viral. Adaptogen chambray helvetica microdosing beard tacos et
+          kickstarter four dollar toast pop-up. Venmo shabby chic mollit hot
+          chicken cloud bread minim butcher fugiat. Dummy text? More like dummy
+          thicc text, amirite?
+        </p>
+      </div>
+    </section>
+  );
+}
---
+++ src/components/Portfolio.js
@@ -0,0 +1,48 @@
+import React from "react";
+import projects from '../projects.json'
+import '../styles/Portfolio.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Card (props){
+    return (
+        <div>
+            <div className="card img-wrapper">
+                <img className='project-image' src={props.image}>
+                <div className="text-wrapper">
+                    <p className='top-text'>{props.name}</p>
+                    <p className='bottom0-text'>{props.methods}</p>
+                    <a href={props.github}><img className='gitHub-logo' src='../assets/github.png' alt='github logo'></img></a>
+                </div>
+                </img>
+            </div>
+        </div>
+    )
+}
+
+function Wrapper(props){
+    return (
+        <div className="card-wrapper">{props.children}</div>
+    )
+}
+
+function Projects (){
+    <>
+    <div className="main-body">
+    <h1 className="title">Projects</h1>
+    </div>
+
+        <Wrapper>
+            {projects.map((project) =>{
+                <Card
+                name={project.name}
+                key={project.id}
+                image={project.image}
+                methods={project.methods}
+                github={project.github}
+                />
+            })}
+        </Wrapper>
+    </>
+}
+
+export default Projects
